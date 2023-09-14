829321986bf0eced5178c83779ccbc29a597e2e8
Baihaki Kuncara <baihakidwi@gmail.com>
2023-09-15 06:21:56 +0700

    - safe cart into sqflite

--- some_awesome_store/lib/main.dart
+++ some_awesome_store/lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:path/path.dart';
+import 'package:some_awesome_store/models/cart_notifier.dart';
 import 'package:some_awesome_store/screens/screens_home.dart';
+import 'package:sqflite/sqflite.dart';
 
 final dio = Dio();
+late final database;
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(
     const ProviderScope(
       child: MainApp(),
@@ -13,8 +18,19 @@ void main() {
   );
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends ConsumerStatefulWidget {
   const MainApp({super.key});
+
+  @override
+  ConsumerState<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends ConsumerState<MainApp> {
+  @override
+  void initState() {
+    super.initState();
+    setupDatabase();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,4 +42,15 @@ class MainApp extends StatelessWidget {
       ),
     );
   }
+}
+
+void setupDatabase() async {
+  database = await openDatabase(
+    join(await getDatabasesPath(), 'cart_database.db'),
+    onCreate: (db, version) {
+      return db
+          .execute('CREATE TABLE cart(id INTEGER PRIMARY KEY, amount INT)');
+    },
+    version: 1,
+  );
 }
--- some_awesome_store/lib/models/cart_notifier.dart
+++ some_awesome_store/lib/models/cart_notifier.dart
@@ -1,13 +1,25 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:some_awesome_store/main.dart';
+import 'package:sqflite/sqflite.dart';
 
 final cartNotifierProvider =
     StateNotifierProvider<CartNotifier, List<CartItem>>(
         (ref) => CartNotifier());
 
 class CartNotifier extends StateNotifier<List<CartItem>> {
-  CartNotifier() : super([]);
+  CartNotifier() : super([]) {
+    retrieveData();
+  }
 
-  void addItem(CartItem newItem) {
+  void retrieveData() async {
+    final db = await database;
+    final List<Map<String, dynamic>> items = await db.query('cart');
+    for (var item in items) {
+      addItem(CartItem(item['id'], item['amount']), false);
+    }
+  }
+
+  void addItem(CartItem newItem, [bool insertToDatabase = true]) {
     int amount = 0;
     for (final item in state) {
       if (item.id == newItem.id) {
@@ -16,7 +28,20 @@ class CartNotifier extends StateNotifier<List<CartItem>> {
         break;
       }
     }
-    state = [...state, CartItem(newItem.id, newItem.amount + amount)];
+    CartItem tempItem = CartItem(newItem.id, newItem.amount + amount);
+    state = [...state, tempItem];
+    if (insertToDatabase) {
+      addItemToDatabase(tempItem);
+    }
+  }
+
+  void addItemToDatabase(CartItem item) async {
+    final db = await database;
+    db.insert(
+      'cart',
+      item.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
   }
 
   void removeItem(CartItem newItem) {
@@ -24,6 +49,16 @@ class CartNotifier extends StateNotifier<List<CartItem>> {
       for (final item in state)
         if (newItem.id != item.id) item
     ];
+    removeItemFromDatabase(newItem.id);
+  }
+
+  void removeItemFromDatabase(int id) async {
+    final db = await database;
+    db.delete(
+      'cart',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
   }
 }
 
@@ -36,5 +71,12 @@ class CartItem {
   @override
   String toString() {
     return '$id:$amount';
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'amount': amount,
+    };
   }
 }
